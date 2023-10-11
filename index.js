9c2294cd3190c8017f42e7b8ca9d07a66ace835a
OGECHI-BEN <123761516+OGECHI-BEN@users.noreply.github.com>
2023-10-11 13:29:06 +0100

    edited work
    edited the colors, made it possible to add ,edit and delete task content

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
- const plans = ['get my transcript','take Agro related courses','Join Dekemp','actively volunteer','Take German on Duolingo', 'write Ielts', 'get a scholarship', 'go to Germany','graduate with a first class'];
- let current_edit_taskId =0;
+ const plans = ['get my transcript','Disable my whatsapp','take Agro related courses','Join a mentorship platform','actively volunteer','Take German on Duolingo', 'write Ielts', 'get a scholarship', 'go to Germany','graduate with a first class'];
+ 
 //   for (let i=0;i< plans.length; i++ ){
 //      console.log(plans[i]);
 //   };
 
-
  // adding task to the input
-function addTask (){
+function addTask (){ 
    let newTask = document.getElementById('input_bar').value
-   plans.push(newTask);
-   // console.log(plans);
-   updateUI;
+   if (newTask == ''){
+      alert('please add a task')
+   } else {
+      plans.push(newTask);
+   };
+    console.log(plans);
+    document.getElementById('input_bar').value = '';
+   updateUI();
+   saveData();
+
  }
 
 
@@ -22,14 +28,14 @@ function addTask (){
                               ${plans[i]}
                               <div class="icons">
                                  <span ><img src="pen-solid.svg" alt="" onclick ="OpenEditTask(${i})"></span> 
-                                 <span ><img src="trash-can-regular.svg" alt="" onclick ="cancel_task()"></span> 
+                                 <span ><img src="trash-can-regular.svg" alt="" onclick ="deleteTask(${i})"></span> 
                               </div>
                            </div>
                            <div id ="while_editing_${i}" class="while_editing">
-                              <input id="edit_bar" type = "text" placeholder = "edit task">
+                              <input id="edit_bar_${i}" class ="edit_bar" type = "text" placeholder = "edit task">
                               <div class= "icons">
-                                 <span ><img src="check-solid.svg" alt=""onclick ="saveEditedTask()" ></span> 
-                                 <span ><img src="xmark-solid.svg" alt="" onclick ="cancelEditedTask()"></span> 
+                                 <span ><img src="check-solid.svg" alt=""onclick ="saveEditedTask(${i})" ></span> 
+                                 <span ><img src="xmark-solid.svg" alt="" onclick ="cancelEditedTask(${i})"></span> 
                               </div>
                            </div>
                         </div>`
@@ -44,25 +50,37 @@ function addTask (){
 // opening the edit bar
 
  function   OpenEditTask (task_id){
-   document.getElementById(`while_editing_${task_id}`).style.display = 'block';
+   document.getElementById(`edit_bar_${task_id}`).value = plans[task_id]
+   document.getElementById(`while_editing_${task_id}`).style.display = 'flex';
    document.getElementById(`task_contents_${task_id}`).style.display = 'none';
-   document.getElementById(`task_content_${task_id}`).value
-   
  }
 
- function cancelTask(task_id){
+ function deleteTask(task_id){
    console.log(task_id);
    //delete item
-   plans.splice(task_id,1)
+   plans.splice(task_id,1);
+    console.log(plans);
    //update UI
-   updateUI
-
-
+   updateUI();
  }
-  
- function saveEditedTask(){
+  function cancelEditedTask (task_id){
+   document.getElementById(`edit_bar_${task_id}`).value ="";
+  }
+ function saveEditedTask(task_id){
+   plans[task_id] = document.getElementById(`edit_bar_${task_id}`).value;
+  //let rat = document.getElementById(`edit_bar_${task_id
 
- }
+  updateUI();
+  saveData()
+
+}
+
+function saveData (){
+   localStorage.setItem("data", task_items.innerHTML);
+
+}
+
  
 
+ 
  
